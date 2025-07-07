12ee37edaa2ff2e4adefe12675a78d3af3dc8168
Jay Limbachiya <jaylimbachiya43@gmail.com>
2025-07-07 12:01:13 +0530

    Clean: Removed node_modules and other ignored files from Git

--- dist/api/route.d.ts
+++
@@ -1,6 +0,0 @@
-import { NextResponse } from 'next/server';
-export declare function chatHandler(req: Request): Promise<NextResponse<{
-    error: string;
-}> | NextResponse<{
-    response: string;
-}>>;
